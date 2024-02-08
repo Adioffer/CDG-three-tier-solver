6f6f2f544554f77228d3396f53a26d613764f968
Adi Offer <offera@post.bgu.ac.il>
2024-02-09 00:00:41 +0200

    Calculate geolocation RMSE + pretty-print

--- src/CDGeB1/main.py
+++ src/CDGeB1/main.py
@@ -1,5 +1,9 @@
 from geolocation_in_aws import Geolocation
+import numpy as np
+from sklearn.metrics import mean_squared_error
 from plot_map import MapBuilder
+from rich.console import Console
+from rich.table import Table
 
 from common import cdgeb_probes, cdgeb_frontends, cdgeb_files
 from common import probeId2Name, frontendId2Name, fileId2Name
@@ -7,32 +11,55 @@ from common import frontend_locations, real_fe_for_file, real_file_for_fe
 from common import aws_distances, aws_delays
 
 
-def test_for_server(frontend):
+def geolocate_frontend(frontend):
     filename = real_file_for_fe[frontend]
     delays_from_server = {frontend_name:aws_delays[(frontend_name, filename_d)] for (frontend_name, filename_d) in aws_delays.keys() \
                         if filename_d == filename}
     delays_from_server.pop(frontend)    
     
-    target = Geolocation.geolocate_target(delays_from_server, frontend_locations)
-    # print('Estimation of server', frontend, 'is', target)
-    
-    closest = Geolocation.closest_frontend(target, frontend_locations)
-    # print('Closest server to estimation:', closest)
-    
+    # Geolocation
+    estimated_location = Geolocation.geolocate_target(delays_from_server, frontend_locations)
+    closest_frontend = Geolocation.closest_frontend(estimated_location, frontend_locations)
+
+    # Make map file
     map = MapBuilder(f'{frontend}_estimated')
     map.add_frontends()
-    map.add_point(target, f'estimated-location-of-{frontend}')
-    map.add_circle(target, Geolocation.haversine(target, frontend_locations[frontend]))
+    map.add_point(estimated_location, f'estimated-location-of-{frontend}')
+    map.add_circle(estimated_location, Geolocation.haversine(estimated_location, frontend_locations[frontend]))
     map.save_map()
 
-    return closest
+    return estimated_location, closest_frontend
 
 if __name__ == '__main__':
-    results = dict()
-    for frontend in cdgeb_frontends:
-        closest = test_for_server(frontend)
-        results[frontend] = closest
+    # Create a Rich table
+    table = Table(title="Geolocation Results", show_header=True, header_style="bold cyan")
 
-    [print(k, ":", k == v, ", error (km): ", round(Geolocation.haversine(frontend_locations[k], frontend_locations[v]), 2), 
-           f'[{v}]') \
-     for k,v in results.items()]
+    table.add_column("Taget", justify="center")
+    table.add_column("Geolocation Error \[km]", justify="center")
+    table.add_column("Closest Frontend", justify="center")
+    table.add_column("Closest Frontend Error \[km]", justify="center")
+
+    results = dict()
+    errors = list()
+    for frontend in cdgeb_frontends:
+        estimated_location, closest_frontend = geolocate_frontend(frontend)
+
+        geolocation_error = Geolocation.haversine(frontend_locations[frontend], estimated_location)
+        closest_error = Geolocation.haversine(frontend_locations[frontend], frontend_locations[closest_frontend])
+
+        errors.append((geolocation_error, closest_error))
+        table.add_row(frontend, 
+                      str(round(geolocation_error, 2)), \
+                      str(closest_frontend == frontend), \
+                      str(round(closest_error, 2))
+                    )
+
+    # Print the table
+    console = Console()
+    console.print(table)
+
+    mse_error = np.sqrt(np.mean(np.square([err[0] for err in errors])))
+    print("Geolocation Error (MSE): ", round(mse_error, 2), "[km]")
+    mse_error = np.sqrt(np.mean(np.square([err[1] for err in errors])))
+    print("Closest Geolocation Error (MSE): ", round(mse_error, 2), "[km]")
+    print()
